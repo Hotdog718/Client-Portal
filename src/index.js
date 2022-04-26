394cc5a00d51bbe2da0d51d715fe7209ec7b20b6
Alexander <abertholf7626@floridapoly.edu>
2022-04-25 21:51:18 -0400

    Functionality

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const crypto = require('crypto');
 const express = require('express');
 const session = require('express-session');
-const { Database, sha256, generateRandomID } = require('./utils');
+const { Database, sha256, generateRandomID, isDoctor } = require('./utils');
 const bodyParser = require('body-parser');
 const path = require('path');
+const PORT = process.env.PORT || 8000;
 
 const app = express();
+
 const db = new Database({
 	host: process.env.HOST,
 	user: process.env.USER,
@@ -15,7 +17,10 @@ const db = new Database({
 	database: process.env.DATABASE
 });
 
-const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => `Listening on port ${PORT}`);
+
+// Routes
+const chatRoute = require('./routes/chat')(db, server);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -108,17 +113,27 @@ app.use('/logout', (req, res) => {
 	res.redirect('/');
 });
 
-app.get('/chat', async (req, res) => {
-	res.render('chat', { auth: req.session.login });
-});
+// Handle chat
+app.use('/chat', chatRoute);
 
 // Patient Web Pages
 app.all('/request-refill', async (req, res) => {
 	if(req.method !== 'POST') {
-		return res.render('request-refill', { auth: req.session.login });
+		const patientPrescriptions = await db.run('SELECT prescriptionNum, prescriptionName, doctorID, (SELECT username FROM logininfo WHERE prescriptions.doctorID = logininfo.userID) AS username FROM prescriptions WHERE patientID = ?', req.session.login);
+
+		return res.render('request-refill', { auth: req.session.login, patientPrescriptions });
 	}
 	
-	res.redirect('/');
+	const { prescription, reason_refill } = req.body;
+
+	if(prescription === -1) {
+		res.redirect('/?message=Error');
+		return;
+	}
+
+	db.run('UPDATE prescriptions SET needsRefill = TRUE, reason = ?, refillApproved = NULL WHERE prescriptionNum = ?', reason_refill, prescription)
+		.then(() => res.redirect('/?message=Successfully\x20requested\x20prescription'))
+		.catch(() => res.redirect('/?message=Failed\x20to\x20send\x20request'));
 });
 
 app.get('/my-info', async (req, res) => {
@@ -135,23 +150,33 @@ app.get('/my-info', async (req, res) => {
 
 app.all('/create-appointment', async (req, res) => {
 	if(req.method !== 'POST') {
-		return res.render('appointment', { auth: req.session.login });
+		const users = await db.run('SELECT username, userID FROM logininfo WHERE userID != ?', req.session.login);
+		const doctors = users.filter(user => isDoctor(user.userID));
+
+		return res.render('appointment', { auth: req.session.login, doctors });
 	}
 	
-	res.redirect('/?message=appointment created, I guess');
+	const { doctor, date, time, reason } = req.body;
+
+	await db.run('INSERT INTO appointments VALUES (?, ?, ?, ?, ?)', generateRandomID(), req.session.login, doctor, date + ' ' + time, reason)
+		.then(() => res.redirect('/?message=appointment created, I guess'))
+		.catch(() => res.redirect('/create-appointment?message=There\x20was\x20an\x20error\x20try\x20again'));
 });
 
 app.all('/incident-form', async (req, res) => {
 	if(req.method !== 'POST') {
 		return res.render('incident-form', { auth: req.session.login });
 	}
-	
-	res.redirect('/incident-form');
+
+	const { date, time, information } = req.body;
+	await db.run('INSERT INTO incidents (userID, incidentID, datetimeOfIncident, content, isResolved) VALUES (?, ?, ?, ?, NULL)', req.session.login, generateRandomID(), date + ' ' + time, information)
+		.then(() => res.redirect('/?message=Incident\x20has\x20been\x20filed'))
+		.catch(() => res.redirect('/incident-form?message=Sorry\x20there\x20was\x20an\x20error'));
 });
 
 // Doctor Pages
 app.use((req, res, next) => {
-	if(!req.session?.login?.toString().padStart(10, '0').startsWith('1')) {
+	if(!isDoctor(req.session.login)) {
 		res.redirect('/');
 		return;
 	}
@@ -159,33 +184,57 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.all('/incident-response', async (req, res) => {
-	if(req.method !== 'POST') {
-		// Would need to retrieve incidents from DB
-		return res.render('incident-response', { auth: req.session.login });
-	}
-	
-	res.redirect('/');
+app.get('/incident-response', async (req, res) => {
+	// Retrieve incidents from DB
+	const incidents = await db.run('SELECT incidentID, datetimeOfIncident, content, isResolved FROM incidents WHERE isResolved IS NULL');
+
+	return res.render('incident-response', { auth: req.session.login, incidents });
 });
 
-app.all('/incidents/:incidentID', async (req, res) => {
-	if(req.method !== 'POST') {
-
-		return;
+app.post('/incident-response/:incidentID', async (req, res) => {
+	const { incidentID } = req.params;
+	const handle = () => {
+		switch(req.body.resolve) {
+			case 'resolve': return db.run('UPDATE incidents SET isResolved = TRUE WHERE incidentID = ?', incidentID);
+			case 'reject': return db.run('UPDATE incidents SET isResolved = FALSE WHERE incidentID = ?', incidentID);
+			default: return null;
+		}
 	}
 
-	res.redirect('/incident-response')
+	if(req.body.resolve === 'resolve' || req.body.resolve === 'reject') {
+		return await handle()
+			.then(() => res.redirect('/incident-response?message=Responded\x20or\x20something'))
+			.catch(() => res.redirect('/incident-response?message=Failed\x20to\x20resolve'));
+	}
+
+	res.redirect('/incident-response');
 })
 
-app.all('/patient-refills', async (req, res) => {
-	if(req.method !== 'POST') {
-		return res.render('refill-ack', { auth: req.session.login });
+app.get('/patient-refills', async (req, res) => {
+	const refills = await db.run('SELECT prescriptionNum, fname, lname, prescriptionName, reason, dateOfLastRefill FROM prescriptions JOIN patientdata ON patientdata.userID = prescriptions.patientID WHERE doctorID = ? AND refillApproved IS NULL AND needsRefill = true', req.session.login);
+
+	return res.render('refill-ack', { auth: req.session.login, refills });
+});
+
+app.post('/patient-refills/:prescriptionNum', async (req, res) => {
+	const { prescriptionNum } = req.params;
+
+	const handle = () => {
+		switch(req.body.response) {
+			case 'accept': return db.run('UPDATE prescriptions SET refillApproved = TRUE, needsRefill = FALSE WHERE prescriptionNum = ?', prescriptionNum);
+			case 'reject': return db.run('UPDATE prescriptions SET refillApproved = FALSE, needsRefill = FALSE WHERE prescriptionNum = ?', prescriptionNum);
+			default: return null;
+		}
 	}
 
-	// Do something with form data
-	
-	res.redirect('/');
-});
+	if(req.body.response === 'accept' || req.body.response === 'reject') {
+		return await handle()
+			.then(() => res.redirect('/patient-refills?message=Responded\x20or\x20something'))
+			.catch(() => res.redirect('/patient-refills?message=Failed\x20to\x20resolve'));
+	}
+
+	res.redirect('/patient-refills');
+})
 
 app.get('/patient-info', async (req, res) => {
 	const patients = await db.run('SELECT fname, lname, bloodtype, height, weight, hasHadSurgery, additionalNotes FROM medicalinfo JOIN patientdata using(userID)')
@@ -193,11 +242,8 @@ app.get('/patient-info', async (req, res) => {
 	res.render('view-patient-info', { auth: req.session.login, patients });
 })
 
-app.get('/doctor-appointments', (req, res) => {
-	// Fetch appointments from database
+app.get('/doctor-appointments', async (req, res) => {
+	const appointments = await db.run('SELECT fname, lname, reason, datetimeOfAppt FROM appointments JOIN patientdata ON patientdata.userID = appointments.patientID WHERE doctorID = ?', req.session.login);
 
-	res.render('appointments', { auth: req.session.login });
+	res.render('appointments', { auth: req.session.login, appointments });
 });
-
-// Listen on given port
-app.listen(PORT, () => `Listening on port ${PORT}`);
